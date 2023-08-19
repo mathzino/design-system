bf83dc8378f86a0b999b732027d81ad8b6f18e42
mathzino <fairusramadhani29@gmail.com>
2023-08-19 13:34:17 +0700

    fix: style tooltip

--- src/components/molecules/tooltip/tooltip.stories.tsx
+++ src/components/molecules/tooltip/tooltip.stories.tsx
@@ -1,5 +1,5 @@
-import Button, { ESizeButton } from "../Button/Button";
-import Tooltip, { TVariantTooltip } from "./tooltip";
+import Button from "../../atoms/Button/Button";
+import Tooltip from "./tooltip";
 import { Meta, StoryObj } from "@storybook/react";
 
 const meta: Meta<typeof Tooltip> = {
@@ -20,6 +20,6 @@ export const Default: Story = {
   args: {
     title: "this tooltipp!!",
     variant: "default",
-    children: <Button title="check button"></Button>,
+    children: <Button>anone</Button>,
   },
 };
--- src/components/molecules/tooltip/tooltip.tsx
+++ src/components/molecules/tooltip/tooltip.tsx
@@ -12,19 +12,17 @@ interface TooltipProps extends React.HTMLAttributes<HTMLDivElement>, VariantProp
 }
 
 export const tooltipVariants = cva(
-  "absolute max-w-sm truncate bg-[#f4f4f4] text-[#151A1E] text-sm  px-3 py-1.5 rounded-lg invisible font-medium ring-1 ring-slate-300 group-hover:visible opacity-0 group-hover:opacity-100 transition -top-[60%] ease-in delay-300",
+  "absolute max-w-sm truncate bg-[#f4f4f4] text-[#151A1E] text-sm  px-3 py-1.5 rounded-lg invisible font-medium ring-1 ring-slate-300 group-hover:visible opacity-0 group-hover:opacity-100 transition  ease-in delay-300 -top-4",
   {
     variants: {},
     defaultVariants: {},
   }
 );
 
-const parentStyle: string = "group relative top-11 left-28 inline-flex flex-col justify-center items-center cursor-pointer p-[7px] text-xl font-medium";
-
 const Tooltip: React.FC<TooltipProps> = ({ className, title, children, ...props }) => {
   return (
-    <div className={parentStyle}>
-      <span className={cn(tooltipVariants({}), className)}>{title}</span>
+    <div className={"group  inline-flex flex-col justify-center items-center cursor-pointer p-[7px] text-xl font-medium"}>
+      <span className={cn(tooltipVariants({}))}>{title}</span>
       {children}
     </div>
   );
