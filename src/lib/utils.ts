0b329bd74d9c5fe78477bad97360bae7fd6ccdca
fairus <fairusramadhani29@gmail.com>
2024-01-04 19:30:23 +0700

    fix: breadcumbs logic href

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
 import { ClassValue, clsx } from "clsx";
+import { TBreadcrumbsItem } from "src/Components/molecules/Breadcrumb/Breadcrumb";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+interface IJointPath {
+  index: number;
+  breadcrumbs: TBreadcrumbsItem[];
+}
+export const joinPath = ({ index, breadcrumbs }: IJointPath): string => {
+  let path = "";
+  for (let i = 0; i <= index; i++) {
+    path = path + "/" + breadcrumbs[i].path;
+  }
+  return path;
+};
